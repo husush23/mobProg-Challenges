aac271f29bff9a77b20e2c25fd62f0f08fc4be45
Hussein Hashi <husseinkadare40@gmail.com>
2023-07-20 10:17:23 +0300

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -9,3 +9,24 @@ function twoSum(nums, target) {
   return null;
 }
 console.log(twoSum([3, 3], 6));
+
+// O(n^2) Quadratic
+// O(1)    Constant space complexity
+
+
+
+function twoSum(arr, target) {
+    const map = new Map();
+    for (let i = 0; i < arr.length; i++) {
+      const num1 = arr[i];
+      const num2 = target - num1; //missing
+      if (map.has(num2)) {
+        return [i, map.get(num2)];
+      }
+      map.set(num1, i);
+    }
+    return null
+  }
+
+//   O(n) Linear time complexity
+// O(n) Linear space complexity
